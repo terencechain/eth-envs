82f02461ed3e38984e102c813a41a5cf5125d866
terence tsao <terence@prysmaticlabs.com>
2023-08-11 11:25:18 -0700

    feat: add basic interop for devnet8

--- deneb-interop/run.sh
+++ deneb-interop/run.sh
@@ -3,8 +3,7 @@ set -x
 set -e
 
 CHAINID=32382
-SRCHOME=$HOME/src
-PRYSMSRC=$SRCHOME/prysmaticlabs/prysm
+PRYSMSRC=/Users/t/go/src/github.com/prysmaticlabs/prysm # TODO: change this to your prysm source dir
 
 # do the slow build stuff before computing genesis time
 pushd $PRYSMSRC
@@ -27,68 +26,25 @@ BAZEL_V_CMD=$PRYSMSRC/bazel-bin/cmd/validator/validator_/validator
 V_CMD=$INTEROP_BIN/validator
 cp -f $BAZEL_V_CMD $V_CMD
 
-popd
-
-BLOBUTILSRC=$SRCHOME/inphi/blob-utils
-BLOBUTILSCMD=$BLOBUTILSRC/blob-utils
-pushd $BLOBUTILSRC
-go build -o $BLOBUTILSCMD
-chmod +x $BLOBUTILSCMD
-popd
-
-GENESIS=$(($(date +%s) + 5))
+GENESIS=$(($(date +%s) + 30))
 echo "genesis time: $GENESIS"
 
-GETHEXE=$HOME/src/MariusVanDerWijden/go-ethereum/build/bin/geth
-SCRIPTDIR=$PWD # assumes this is run from the dir where the script lives
+GETHEXE=/Users/t/go/src/github.com/lightclient/go-ethereum/build/bin/geth # TODO: change this to your geth binary
+SCRIPTDIR=/Users/t/eth-envs/deneb-interop # TODO: change this to your eth-ens dir
 
-DATADIR=/var/lib/db/deneb-interop/${GENESIS}
+DATADIR=/Users/t/deneb-interop/${GENESIS} # TODO: change this to your tmp dir for logging and data
 mkdir -p $DATADIR
 
-BLOB1=$DATADIR/blob-1
-BLOB2=$DATADIR/blob-2
-BLOB3=$DATADIR/blob-3
-echo "writing blob 1 to $BLOB1"
-dd if=/dev/urandom bs=32 count=4096 of=$BLOB1
-echo "writing blob 2 to $BLOB3"
-dd if=/dev/urandom bs=32 count=4096 of=$BLOB2
-echo "writing blob 1 to $BLOB3"
-dd if=/dev/urandom bs=32 count=4096 of=$BLOB3
-
 CL_DATADIR_1=$DATADIR/cl-1
-CL_DATADIR_2=$DATADIR/cl-2
 GETHDATA_1=$DATADIR/el-1
 mkdir -p $GETHDATA_1/keystore
-GETHDATA_2=$DATADIR/el-2
-mkdir -p $GETHDATA_2/keystore
 
 LOGDIR=$DATADIR/logs
 mkdir -p $LOGDIR
 CL_LOGS_1=$LOGDIR/beacon-node_1.log
 VAL_LOGS_1=$LOGDIR/validator_1.log
-CL_LOGS_2=$LOGDIR/beacon-node_2.log
 GETH_1_LOG=$LOGDIR/geth_1.log
-GETH_2_LOG=$LOGDIR/geth_2.log
 PID_FILE=$LOGDIR/run-pids
-touch $PID_FILE
-echo "pids written to $PID_FILE"
-
-# clean up all the processes on sigint
-trap cleanup INT
-trap cleanup EXIT
-function cleanup() {
-	$(cat $PID_FILE | cut -d' ' -f1 | xargs kill $1)
-}
-
-function log_pid() {
-	SHELL_PID=$1
-	PROC_NAME=$2
-	OUTER_PID=$(ps -o pid,cmd --ppid=$SHELL_PID | grep 'run.sh' | tail -n1 | awk '{ print $1 }')
-	#OUTER_PID=$(ps --ppid=$SHELL_PID | grep 'run.sh' | tail -n1 | cut -d' ' -f1)
-	GO_PID=$(ps --ppid=$OUTER_PID | tail -n1 | awk '{ print $1 }')
-	echo "$PROC_NAME pid = $GO_PID"
-	echo "$GO_PID # $PROC_NAME" >> $PID_FILE
-}
 
 echo "all logs and stdout/err for each program redirected to log dir = $LOGDIR"
 
@@ -99,16 +55,8 @@ cp $SCRIPTDIR/config.yml $DATADIR/config.yml
 cp $SCRIPTDIR/config.yml $DATADIR/config.yml
 
 cp $SCRIPTDIR/keystore/* $GETHDATA_1/keystore
-cp $SCRIPTDIR/keystore/* $GETHDATA_2/keystore
 GETH_PASSWORD_FILE=$DATADIR/geth_password.txt
 cp $SCRIPTDIR/geth_password.txt $GETH_PASSWORD_FILE
-
-SHANGHAI=$(($GENESIS))
-sed -i  -e 's/XXX/'$SHANGHAI'/' $DATADIR/genesis.json
-echo "shanghai fork time: $SHANGHAI"
-CANCUN=$(($GENESIS + 144))
-sed -i  -e 's/YYY/'$CANCUN'/' $DATADIR/genesis.json
-echo "cancun fork time: $CANCUN"
 
 pushd $PRYSMSRC
 
@@ -118,8 +66,7 @@ $CTL_CMD testnet generate-genesis \
 	--fork=capella --geth-genesis-json-in=$DATADIR/genesis.json --geth-genesis-json-out=$DATADIR/genesis.json \
 	1> $LOGDIR/prysmctl-genesis.stdout 2> $LOGDIR/prymctl-genesis.stderr
 
-echo "beacon-node 1 logs at $CL_LOGS_1"
-setsid $($BC_CMD \
+$BC_CMD \
 	--datadir=$CL_DATADIR_1 \
 	--log-file=$CL_LOGS_1 \
         --min-sync-peers=0 \
@@ -131,26 +78,11 @@ setsid $($BC_CMD \
         --accept-terms-of-use \
         --jwt-secret=$JWT_PATH \
 	--execution-endpoint=http://localhost:8551 \
-	--suggested-fee-recipient=0x0000000000000000000000000000000000000000 --verbosity=debug \
-	1> $LOGDIR/beacon-1.stdout 2> $LOGDIR/beacon-1.stderr) &
-PID_BN1=$!
-log_pid $PID_BN1 "beacon node 1"
+	--suggested-fee-recipient=0x1000000000000000000000000000000000000000 --verbosity=debug \
+	1> $LOGDIR/beacon-1.stdout 2> $LOGDIR/beacon-1.stderr &
 
-echo "validator 1 logs at $VAL_LOGS_1"
-setsid $($V_CMD \
-	--datadir=$CL_DATADIR_1 \
-	--log-file=$VAL_LOGS_1 \
-        --accept-terms-of-use \
-        --interop-num-validators=256 \
-        --interop-start-index=0 \
-	--chain-config-file=$DATADIR/config.yml \
-	1> $LOGDIR/validator-1.stdout 2> $LOGDIR/validator-2.stderr) &
-PID_V1=$!
-log_pid $PID_V1 "validator 1"
-
-echo "geth logs at $GETH_1_LOG"
-$GETHEXE --datadir $GETHDATA_1 init $DATADIR/genesis.json 1> $LOGDIR/geth-init_1.stdout 2> $LOGDIR/geth-init_1.stderr
-setsid $($GETHEXE \
+$GETHEXE --datadir $GETHDATA_1 init $DATADIR/genesis.json 1> $LOGDIR/geth-init_1.stdout
+$GETHEXE \
 	--log.file=$GETH_1_LOG \
 	--http \
         --datadir=$GETHDATA_1 \
@@ -161,68 +93,13 @@ setsid $($GETHEXE \
         --password=$GETH_PASSWORD_FILE \
 	--authrpc.port=8551 \
 	--authrpc.jwtsecret=$JWT_PATH \
-	console \
-	1> $LOGDIR/geth-1.stdout 2> $LOGDIR/geth-1.stderr) &
-PID_GETH_1=$!
-log_pid $PID_GETH_1 "geth 1"
+	1> $LOGDIR/geth-1.stdout 2> $LOGDIR/geth-1.stderr &
 
-#WAITTIME=72
-#WAITTIME=$(($SHANGHAI - $(date +%s)))
-WAITTIME=$(($CANCUN - $(date +%s)))
-echo "sleeping $WAITTIME seconds to wait for cancun fork"
-sleep $WAITTIME
-
-ADDR_BN1=$(grep 'Node started p2p server' $CL_LOGS_1 | sed -E 's/.*multiAddr=\"(.*)\" prefix=.*/\1/')
-echo "beacon-node 2 will peer with beacon-node 1 multiaddr = $ADDR_BN1"
-
-echo "beacon-node 2 logs at $CL_LOGS_2"
-setsid $($BC_CMD \
-	--log-file=$CL_LOGS_2 \
-	--datadir=$CL_DATADIR_2 \
-        --min-sync-peers=1 \
-        --genesis-state=$DATADIR/genesis.ssz \
-        --interop-eth1data-votes \
-        --bootstrap-node= \
-        --chain-config-file=$DATADIR/config.yml \
-        --chain-id=$CHAINID \
+$V_CMD \
+	--datadir=$CL_DATADIR_1 \
+	--log-file=$VAL_LOGS_1 \
         --accept-terms-of-use \
-        --jwt-secret=$JWT_PATH \
-        --execution-endpoint=http://localhost:8552 \
-        --rpc-port=4002 \
-        --p2p-tcp-port=13002 \
-        --p2p-udp-port=12002 \
-        --grpc-gateway-port=3502 \
-        --monitoring-port=8083 \
-	--force-clear-db \
-	--verbosity=debug \
-	--peer=$ADDR_BN1 \
-	1> $LOGDIR/beacon-2.stdout 2> $LOGDIR/beacon-2.stderr) &
-PID_BN2=$!
-log_pid $PID_BN2 "beacon node 2"
-
-echo "geth2 logs at $GETH_2_LOG"
-$GETHEXE --datadir $GETHDATA_2 init $DATADIR/genesis.json 1> $LOGDIR/geth-init_2.stdout 2> $LOGDIR/geth-init_2.stderr
-setsid $($GETHEXE \
-	--log.file=$GETH_2_LOG \
-	--http \
-        --datadir=$GETHDATA_2 \
-        --nodiscover \
-        --syncmode=full \
-        --allow-insecure-unlock \
-        --unlock=0x123463a4b065722e99115d6c222f267d9cabb524 \
-        --password=$GETH_PASSWORD_FILE \
-	--authrpc.jwtsecret=$JWT_PATH \
-	--authrpc.port=8552 \
-	--http.port=8546 \
-	--port=30304 \
-	1> $LOGDIR/geth-2.stdout 2> $LOGDIR/geth-2.stderr) &
-PID_GETH_2=$!
-log_pid $PID_GETH_2 "geth 2"
-
-#$BLOBUTILSCMD tx --blob-file=$BLOB1 --private-key 2e0834786285daccd064ca17f1654f67b4aef298acbb82cef9ec422fb4975622 --to 0x0 --gas-price 100000000000 --gas-limit 1000000 --chain-id 32382 --rpc-url http://localhost:8545
-#$BLOBUTILSCMD tx --blob-file=$BLOB2 --private-key 2e0834786285daccd064ca17f1654f67b4aef298acbb82cef9ec422fb4975622 --to 0x0 --gas-price 100000000000 --gas-limit 1000000 --chain-id 32382 --rpc-url http://localhost:8545
-#$BLOBUTILSCMD tx --blob-file=$BLOB3 --private-key 2e0834786285daccd064ca17f1654f67b4aef298acbb82cef9ec422fb4975622 --to 0x0 --gas-price 100000000000 --gas-limit 1000000 --chain-id 32382 --rpc-url http://localhost:8545
-
-echo "sleeping until infinity or ctrl+c, whichever comes first"
-sleep infinity
-
+        --interop-num-validators=256 \
+        --interop-start-index=0 \
+	--chain-config-file=$DATADIR/config.yml \
+	1> $LOGDIR/validator-1.stdout 2> $LOGDIR/validator-2.stderr &
